4b2921f0ee825d13b9e870bb1c03d6a0e05739df
SHEREEN HASAN <hasan.shereen111@gmail.com>
2019-09-11 21:35:03 -0400

    second commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'mNDieRswBFETdOHzXU2YAV4Af',
+    consumer_secret: '5jdAToIom5MvODaQ9UT728ZnCB58ei7deS6uUiblRwT8jwvK9G',
+    access_token_key: '1404092737-Xl6BThdYcezGTbQpOmUvhOAJwsKtLoHk51CSONL',
+    access_token_secret: 'M3p6KZh0uBQ0KvuFfPdGNesjKFLx9r8R1GGIt46XRThpF'
+  }
